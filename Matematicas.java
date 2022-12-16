4e66c31dd57cc671bbbcc288065f59d97af2f7a4
pfer <admin@pfer.es>
2022-12-16 13:42:41 +0100

    version 0.0.4, se han creado dos clases para saber si es divisible de 3 y 5

--- Matematicas.java
+++ Matematicas.java
@@ -17,5 +17,28 @@ public class Matematicas {
 		
 	return x;
 	}
+	public static boolean esDivisiblePorTres(int numero){
+	boolean x;
+	if (numero % 3 == 0){
+	x = true;
+}
+	else{
+	x = false;
+	}
+		
+	return x;
+	}
+	public static boolean esDivisiblePorCinco(int numero){
+	boolean x;
+	if (numero % 5 == 0){
+	x = true;
+}
+	else{
+	x = false;
+	}
+		
+	return x;
+	}
+	
 }
 
--- TestMatematicas.java
+++ TestMatematicas.java
@@ -17,7 +17,22 @@ public class TestMatematicas{
 	}else{
 		System.out.printf("El numero %d es impar",dato);
 	}
-
+	System.out.println("Dime el dato para comprobar si es divisible de 3 \n");
+	int dato2 = sc.nextInt();
+	par = Matematicas.esDivisiblePorTres(dato2);
+	if (par == true){
+		System.out.printf("El numero %d es divisible de 3",dato2);
+	}else{
+		System.out.printf("El numero %d es divisible de 3",dato2);
+	}
+		System.out.println("Dime el dato para comprobar si es divisible de 5\n");
+	int dato3 = sc.nextInt();
+	par = Matematicas.esDivisiblePorCinco(dato3);
+	if (par == true){
+		System.out.printf("El numero %d es divisible de 5",dato3);
+	}else{
+		System.out.printf("El numero %d es divisible de 5",dato3);
+	}
 	
 	}
 }
