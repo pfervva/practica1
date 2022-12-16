35aa728fe85f3131d07e619898532700bdbce73c
pfer <admin@pfer.es>
2022-12-16 13:22:14 +0100

    Version 0.0.2, se ha añadido el metodo al Test y tambien se a solicitado un dato al usuario

--- Matematicas.java
+++ Matematicas.java
@@ -6,6 +6,16 @@
 
 public class Matematicas {
 	
-
+	public static boolean esPar(int numero){
+	boolean x;
+	if (numero % 2 == 0){
+	x = true;
+}
+	else{
+	x = false;
+	}
+		
+	return x;
+	}
 }
 
--- TestMatematicas.java
+++ TestMatematicas.java
@@ -3,11 +3,13 @@
  * 
  * 
  */
-
+import java.util.Scanner;
 
 public class TestMatematicas{
 	
 	public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int dato = sc.nextInt();
 		
 	}
 }
