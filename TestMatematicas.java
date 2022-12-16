b4bba4cf6948689df39eb3095b9980ea634100af
pfer <admin@pfer.es>
2022-12-16 13:27:03 +0100

    Version 0.0.3 donde se toma el valor de la clase en una variable booleana

--- TestMatematicas.java
+++ TestMatematicas.java
@@ -9,8 +9,16 @@ public class TestMatematicas{
 	
 	public static void main (String[] args) {
 	Scanner sc = new Scanner(System.in);
+	System.out.println("Dime el dato para comprobar si es par o impar");
 	int dato = sc.nextInt();
-		
+	boolean par = Matematicas.esPar(dato);
+	if (par == true){
+		System.out.printf("El numero %d es par",dato);
+	}else{
+		System.out.printf("El numero %d es impar",dato);
+	}
+
+	
 	}
 }
 
